64744335b1bddb0056665cc278ce45b86e4ad6d0
Dhrubesh Deb Sharma <dhrubesh97@gmail.com>
2020-05-25 16:18:48 +0530

    adds a new state so that signup state isnt shown when logged in unless the firebase promise gets resolved

--- src/components/Authenticate.js
+++ src/components/Authenticate.js
@@ -2,19 +2,26 @@ import React, { useState } from "react";
 import fire from "../config/FireBase";
 import AdminPortal from "./AdminPortal";
 import SignIn from "./AdminLogin";
+import { LinearProgress } from '@material-ui/core';
 
 function Authenticate() {
+  const showSignInState = 'showSignIn';
   const [user, setUser] = useState(null);
 
   fire.auth().onAuthStateChanged((user) => {
     if (user) {
       setUser(user);
     } else {
-      setUser(null);
+      setUser(showSignInState);
     }
   });
 
-  return <>{user ? <AdminPortal /> : <SignIn />}</>;
+
+  if (!user) {
+    return <LinearProgress  />
+  }
+
+  return <>{user === showSignInState ? <SignIn /> : <AdminPortal />}</>;
 }
 
 export default Authenticate;

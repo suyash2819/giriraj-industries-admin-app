a12c22e7e7afe0d5a7e089b8b2f7d4c09403ce6c
suyash mandhana <mandhanasuyash@yahoo.in>
2020-06-21 01:18:25 +0530

    Removed Repetitive code, changed file name RenderFor

--- src/components/AdminPortal.js
+++ src/components/AdminPortal.js
@@ -1,81 +1,55 @@
 import React, { useState } from "react";
 import { fire } from "../config/FireBase";
 import "../CSS/AdminPortal.css";
-import RenderForm from "./RenderForm";
+import RenderForm from "./ItemDisplayForm";
 import Button from "@material-ui/core/Button";
+import { sections } from "./Data";
+
+const initialFormValue = {
+  [sections.men]: false,
+  [sections.women]: false,
+  [sections.kids]: false,
+  [sections.covid]: false,
+};
 
 const AdminPortal = () => {
-  const [ShowMenForm, setShowMenForm] = useState(false);
-  const [ShowWomenForm, setShowWomenForm] = useState(false);
-  const [ShowKidsForm, setShowKidsForm] = useState(false);
-  const [ShowCovidForm, setShowCovidForm] = useState(false);
+  const [showForms, setShowForms] = useState(initialFormValue);
+  const sectionArray = [
+    { title: "Men", val: sections.men },
+    { title: "Women", val: sections.women },
+    { title: "Kids", val: sections.kids },
+    { title: "Covid", val: sections.covid },
+  ];
 
   const logout = () => {
     fire.auth().signOut();
   };
 
   const updateState = (category) => {
-    if (category === "Men") {
-      setShowMenForm(true);
-    } else if (category === "Women") {
-      setShowWomenForm(true);
-    } else if (category === "Kids") {
-      setShowKidsForm(true);
-    } else if (category === "Covid") {
-      setShowCovidForm(true);
-    }
+    let obj = { ...showForms };
+    obj[category] = true;
+    console.log(obj);
+    setShowForms(obj);
   };
 
   return (
     <>
-      <div className="Heading">
-        <h1>Men</h1>
-        {ShowMenForm ? <RenderForm db="Men" /> : null}
-        <Button
-          variant="contained"
-          color="primary"
-          component="span"
-          onClick={() => updateState("Men")}
-        >
-          Add Item
-        </Button>
-      </div>
-      <div className="Heading">
-        <h1>Women</h1>
-        {ShowWomenForm ? <RenderForm db="Women" /> : null}
-        <Button
-          variant="contained"
-          color="primary"
-          component="span"
-          onClick={() => updateState("Women")}
-        >
-          Add Item
-        </Button>
-      </div>
-      <div className="Heading">
-        <h1>Kids</h1>
-        {ShowKidsForm ? <RenderForm db="Kids" /> : null}
-        <Button
-          variant="contained"
-          color="primary"
-          component="span"
-          onClick={() => updateState("Kids")}
-        >
-          Add Item
-        </Button>
-      </div>
-      <div className="Heading">
-        <h1>Covid</h1>
-        {ShowCovidForm ? <RenderForm db="Covid" /> : null}
-        <Button
-          variant="contained"
-          color="primary"
-          component="span"
-          onClick={() => updateState("Covid")}
-        >
-          Add Item
-        </Button>
-      </div>
+      {sectionArray.map((section, index) => {
+        return (
+          <div className="Heading" key={index}>
+            <h1>{section.title}</h1>
+            {showForms[section.val] ? <RenderForm db={section.val} /> : null}
+            <Button
+              variant="contained"
+              color="primary"
+              component="span"
+              onClick={() => updateState(section.val)}
+            >
+              Add Item
+            </Button>
+          </div>
+        );
+      })}
       <hr></hr>
       <center>
         <Button
---
+++ src/components/Data.js
@@ -0,0 +1,7 @@
+export const sections = {
+  men: "Men",
+  women: "Women",
+  kids: "Kids",
+  covid: "Covid",
+};
+// export const sections = ["Men", "Women", "Kids", "Covid"];
